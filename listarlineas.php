95a40810cec7fa12f4dd3837a22b04e5558abbc4
ALEX ABRAHAM ROITMAN <41758511@ort>
2016-07-13 16:29:48 -0300

    listar

--- listarlineas.php
+++ listarlineas.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost","u241352082_alexr","123456","u241352082_bdfin");
+$con=mysqli_connect("us-cdbr-azure-central-a.cloudapp.net","b0f137b65248c5","b0f137b65248c5","u241352082_bdfin");
+// $con=mysqli_connect("localhost","u241352082_alexr","123456","u241352082_bdfin");
 if (mysqli_connect_errno()) {
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
